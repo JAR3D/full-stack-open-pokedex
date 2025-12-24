62631183b7d6f39b310d43b744e40344f93fda7b
Joao Rosado <joaorosado@Joaos-Air-2.home>
2025-12-24 09:22:30 +0000

    feat: add version endpoint for deployment checks

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ const PORT = process.env.PORT || 5001
 
 app.use(express.static('dist'))
 
+// dummy endpoint in the app that makes it possible to do some code changes 
+// and to ensure that the deployed version has really changed
+app.get('/version', (req, res) => {
+  res.send('1');
+})
+
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`server started on port ${PORT}`)
